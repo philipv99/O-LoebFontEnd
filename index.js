7f880b43e0064794254c78b0643d571dff5fba90
philipv99 <philipv9913@gmail.com>
2023-12-08 01:00:24 +0100

    fixed run site and post is done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ Vue.createApp({
    data(){
       return{
          showAddPostForm: false,
+         RunTypsList: ["O-løb","Stjerne-løb"],
          ListOfPosts: [{
             "id": 3,
             "name": "roskirke",
@@ -13,46 +14,8 @@ Vue.createApp({
             "sadius": 23,
             "gpsLatitude": 23.23232,
             "gpsLongitude": 110.23232,
-            "runId": 0  
-         },
-         {
-            "id": 1,
-            "name": "roskirke",
-            "sequenceNumber": 1,
-            "sadius": 23,
-            "gpsLatitude": 23.23232,
-            "gpsLongitude": 110.23232,
-            "runId": 0  
-         },
-         {
-            "id": 1,
-            "name": "roskirke",
-            "sequenceNumber": 1,
-            "sadius": 23,
-            "gpsLatitude": 23.23232,
-            "gpsLongitude": 110.23232,
-            "runId": 0  
-         },
-         ],
-         addedPosts:[{
-            "id": 0,
-            "name": "roskirke",
-            "sequenceNumber": 1,
-            "sadius": 23,
-            "gpsLatitude": 23.23232,
-            "gpsLongitude": 110.23232,
-            "runId": 0  
-         },
-         {
-            "id": 0,
-            "name": "roskirke",
-            "sequenceNumber": 1,
-            "sadius": 23,
-            "gpsLatitude": 23.23232,
-            "gpsLongitude": 110.23232,
-            "runId": 0  
+            "runId": 0
          }],
-         AddedRunSuccesMessege: "",
          RunToAdd: {
             Id: 0,
             Name: "",
@@ -60,47 +23,45 @@ Vue.createApp({
          },
       };
    },
-   async created(){
-      this.ListOfPosts.push(this.GetAll(GetPostUrl))
-   },
    methods: {
-      async GetAll(Url){
-         try{
-            response = await axios.get(Url)
-            value = await response.data
-            console.log(value)
-            return value
-         }
-         catch (ex){
-            alert(ex.message)
-            console.log(Url, "did not respond")
-         }
-      },
+      // async GetAll(Url){
+      //    try{
+      //       response = await axios.get(Url)
+      //       value = await response.data
+      //       console.log(value)
+      //       return value
+      //    }
+      //    catch (ex){
+      //       alert(ex.message)
+      //       console.log(Url, "did not respond")
+      //    }
+      // },
       async PoshNewRun(){
          try{
             response = await axios.post(PoshRunsUrl, this.RunToAdd).then(response => {console.log(response.status, response.data.token)})
             this.AddedRunSuccesMessege = "Løb blev tilføjet"
             this.RunToAdd.Name = ""
             this.RunToAdd.RunType = ""
+            alert("Run was added")
           }
          catch (ex) {
             alert(ex.message)
          }
       },
-      addPost(object){
-         try {
-            this.addedPosts.push(object)
-            this.ListOfPosts.pop(object)
-            console.log(addedPosts)
-         } catch (error) {
-            console.log(error)
-         }
-         this.showAddPostForm = false
-      },
-      ButtenPress(){
-         this.showAddPostForm = !this.showAddPostForm 
-         console.log("show was pressed:", this.showAddPostForm)
-      }
+      // addPost(object){
+      //    try {
+      //       this.addedPosts.push(object)
+      //       this.ListOfPosts.pop(object)
+      //       console.log(addedPosts)
+      //    } catch (error) {
+      //       console.log(error)
+      //    }
+      //    this.showAddPostForm = false
+      // },
+      // ButtenPress(){
+      //    this.showAddPostForm = !this.showAddPostForm 
+      //    console.log("show was pressed:", this.showAddPostForm)
+      // }
    }
 }).mount("#app")
 
--- post.js
+++ post.js
@@ -1,5 +1,5 @@
 const BaseUrl = "https://o-loebrest20231128112940.azurewebsites.net"
-const GetPostUrl = BaseUrl + "/api/Posts" // + "id"
+const PostUrl = BaseUrl + "/api/Posts" // + "id"
 const RunsUrl = BaseUrl + "/api/Runs"
 
 
@@ -7,6 +7,7 @@ Vue.createApp({
    data(){
       return{
          selectedRunid: -1,
+         indexOfList: 1,
          ListOfRuns: [],
          addedPost:[
       ],
@@ -47,9 +48,89 @@ Vue.createApp({
          this.selectedRunid = item
          console.log(this.selectedRunid)
       },
-      PushPost(){
-         this.post.RunId = selectedRunid
+      async PushPost(){
+         try{
+            await this.addedPost.forEach(item => {
+               response = axios.post(PostUrl, item)
+               console.log(response, response.status)
+            });
+         }
+         catch(error){
+            console.log(error)
+         }
+         console.log("post done: ", response, response.status)
+      },
+      addPostToList(){
+         if(!this.checkPost()){
+            console.log("check 1 fejl")
+            return
+         }
+         if(!this.checkPost2()){
+            console.log("check 2 fejl")
+            return
+         } 
+         if(!this.checkPost3()){
+            console.log("check 3 fejl")
+            return
+         } 
+         if(!this.checkPost4()){
+            console.log("check 4 fejl")
+            return
+         } 
+         else{
+            this.post.RunId = this.selectedRunid.id;
+            this.post.SequenceNumber = this.indexOfList;
+            this.indexOfList = this.indexOfList + 1;
+            let addeditem = {...this.post}
+            this.addedPost.push(addeditem)
+         }
+      },
 
+   // ------------    check  methoder    ---------------- // 
+      checkPost(){
+         if(this.selectedRunid === -1){
+            console.log(this.selectedRunid)
+            alert("Mangler at tilføje løb")
+            return false
+         } if(this.post.GpsLatitude <= -181 && this.post.GpsLatitude >= 181){
+            console.log(this.post.GpsLatitude)
+            alert("breddrad skal være mellem -180 og 180")
+            return false
+         } if(this.post.GpsLongitude <= -91 && this.post.GpsLongitude >= 91){
+            console.log(this.post.GpsLongitude)
+            alert("breddrad skal være mellem -90 og 90")
+            return false
+         }
+         else{
+            return true
+         }
+      },
+      checkPost2(){
+         if(this.post.GpsLatitude < -180 || this.post.GpsLatitude > 180){
+            console.log(this.post.GpsLatitude)
+            alert("bredegrad skal være mellem -180 og 180")
+            return false
+         } else {
+            return true
+         }
+      },
+      checkPost3(){
+         if(this.post.GpsLongitude < -90 || this.post.GpsLongitude > 90){
+            console.log(this.post.GpsLatitude)
+            alert("Længdegrad skal være mellem -90 og 90")
+            return false
+         } else {
+            return true
+         }
+      },
+      checkPost4(){
+         if(this.post.Name.length === 0){
+            console.log(this.post.Name)
+            alert("der er ikke navn på post")
+            return false
+         }else{
+            return true
+         }
       }
    }
 }).mount("#app")
