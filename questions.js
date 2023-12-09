699917fd4ba6cc4c0abd14b6cfeb77917af92976
philipv99 <philipv9913@gmail.com>
2023-12-09 11:35:28 +0100

    added Q's to site not done

--- questions.js
+++ questions.js
@@ -1,6 +1,8 @@
 const BaseUrl = "https://o-loebrest20231128112940.azurewebsites.net"
-const PoshQuestions = BaseUrl + "/api/Questions"
-const PoshAwnsers = BaseUrl + "/api/Awnsers"
+const QuestionsUrl = BaseUrl + "/api/Questions"
+const AwnsersUrl = BaseUrl + "/api/Awnsers"
+const PostUrl = BaseUrl + "/api/Posts" // + "id"
+
 
 Vue.createApp({
    data(){
@@ -12,39 +14,42 @@ Vue.createApp({
             PostId: -1
          },
          Awnsers: {
-            Id: -1,
+            Id: 0,
             AnswerToQuestion: "",
             IsCorrectAnswer: false,
             QuestionId: -1
          },
+         AwnserId: 0,
+         Posts: [],
          tempAwnser1: "",
          tempAwnser2: "",
          tempAwnser3: "",
          tempAwnser4: "",
-         AwnserId: 0,
+         ChosenPost: {},
       };
    },
    async created(){
-      //this.ListOfPosts.push(this.GetAll(PoshRunsUrl))
+      this.GetPost()
    },
    methods: {
-      async GetAll(Url){
+      async GetPost(){
          try{
-            response = await axios.get(Url)
+            response = await axios.get(PostUrl)
             value = await response.data
             console.log(value)
-            return value
+            this.Posts = value
          }
          catch (ex){
-            alert(ex.message)
-            console.log(Url, "did not respond")
+            console.log(Url, "did not respond", ex.message)
          }
       },
       async addQuestion(){
          //this.check()
          try{
-            response = await axios.post(PoshQuestions, this.Question)
-            console.log(response)
+            //response = await axios.post(QuestionsUrl, this.Question)
+            this.Question.Id = 4
+            console.log(this.Question)
+            this.addAnswers(this.Question.Id)
          }
          catch(error) {
             console.log(error)
@@ -52,19 +57,31 @@ Vue.createApp({
          //this.addAnswers(response.id)
          console.log()
       },
-      addAnswers(id){
+      chosenPost(item){
+         this.ChosenPost = item
+         this.Question.PostId = item.id
+         console.log(this.ChosenPost)
+         console.log("id of post",this.Question.PostId)
+      },
+      async addAnswers(id){
          try{
-            
+            for(let i = 0; i < 4; i++){
+               senditem = {...this.Awnsers}
+               senditem.QuestionId = id
+               if(i === AwnserId){
+                  senditem.IsAnswered
+               }
+
+               console.log(senditem)
+            }
          }
          catch (error){
             console.log(error)
          }
       },
-      check(){
-         if (AwnserId == 0){
-            alert("Answer not set")
-            return
-         }
-      }
+      // check(){
+      //   
+         //}
+      //}
    }
 }).mount("#app")
