3a900ce51cbc6bfcc541e64f39036bca6628554c
Christian Robbers Rasmussen <crr@likehero.dk>
2023-12-12 17:22:08 +0100

    Made it so seeruns get all posts and put markers on the map

--- seeruns.js
+++ seeruns.js
@@ -10,59 +10,85 @@ Vue.createApp({
          ListofRuns: [],
          ListofPosts: [],
          FilteredList: [],
-         map: {},
       };
    },
    async created(){
         this.GetAllRuns(RunsUrl)
-        
    },
    methods:
    {
-    async GetAllRuns(url){
-        try{
-            response = await axios.get(url)
-            console.log(response.status, response)
-            this.ListofRuns = response.data
-            }
-            catch(error){
-            console.log(error)
-            }
-            this.GetAllPosts(PostUrl)
+        async GetAllRuns(url){
+            try{
+                response = await axios.get(url)
+                console.log(response.status, response)
+                this.ListofRuns = response.data
+                }
+                catch(error){
+                console.log(error)
+                }
+                this.GetAllPosts(PostUrl)
+            },
+
+        async GetAllPosts(url){
+            try{
+                response = await axios.get(url)
+                console.log(response.status, response)
+                this.ListofPosts = response.data
+                }
+                catch(error){
+                console.log(error)
+                }
+            },
+
+        connectRun(item){
+            this.selectedRun=item.id
+            console.log(this.selectedRun)
+            this.postfilter()
         },
 
-    async GetAllPosts(url){
-        try{
-            response = await axios.get(url)
-            console.log(response.status, response)
-            this.ListofPosts = response.data
-            }
-            catch(error){
-            console.log(error)
-            }
+        postfilter(){
+            this.FilteredList=""
+            this.FilteredList=this.ListofPosts.filter(x => x.runId===this.selectedRun)
+            this.FilteredList.sort((a,b) => {
+                return a.sequenceNumber - b.sequenceNumber
+            })
+            console.log(this.FilteredList)
         },
 
-    connectRun(item){
-        this.selectedRun=item.id
-        console.log(this.selectedRun)
-        this.postfilter()
-    },
+        initializeMap() {
+            const filteredPosts = this.ListofPosts.filter(x => x.runId === this.selectedRun);
 
-    postfilter(){
-        this.FilteredList=""
-        this.FilteredList=this.ListofPosts.filter(x => x.runId===this.selectedRun)
-        this.FilteredList.sort((a,b) => {
-            return a.sequenceNumber - b.sequenceNumber
-        })
-        console.log(this.FilteredList)
-    },
+            
+            let centerPoint = [55.6310684, 12.0780378]; // Default set to Roskilde
 
-    addmarker(map){
-        const filteredPost = this.ListofPosts.filter(x => x.runId===this.selectedRun)
-        filteredPost.forEach(p => {
-            new tt.Marker().setLngLat(p.gpsLongitude, p.gpsLatitude).addTo(map)
-            this.addmarker(map)
-        });
+            for (const p of filteredPosts) {
+                if (p.sequenceNumber === 1) {
+                    centerPoint = [p.gpsLongitude, p.gpsLatitude];
+                    break; // Exit the loop once a center with sequence number 1 is found
+                }
+            }
+
+            let center = centerPoint;
+            
+            
+            
+            console.log("Center location" + center)
+
+            map = tt.map({
+                key: "vEdHLNMgoA1msNHdxlnxOW7fbO2vcDZC",
+                container: "map",
+                center: center,
+                zoom: 10,
+            });
+            map.on("load", () => {
+                
+                
+                filteredPosts.forEach(post => {
+                    new tt.Marker().setLngLat([post.gpsLongitude, post.gpsLatitude]).addTo(map);
+                });
+                
+            });
+        },
     },
-   }
 }).mount("#app")
+
